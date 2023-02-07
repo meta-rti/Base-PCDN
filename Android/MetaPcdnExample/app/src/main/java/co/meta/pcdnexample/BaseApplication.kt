d199b711c581b6b1907d13fa4e075b366e288b81
zhangchunpng <sharetwodroid@gmail.com>
2023-02-07 14:34:24 +0800

    android pcdn example
    Signed-off-by: zhangchunpng <sharetwodroid@gmail.com>

---
+++ Android/MetaPcdnExample/app/src/main/java/co/meta/pcdnexample/BaseApplication.kt
@@ -0,0 +1,22 @@
+package co.meta.pcdnexample
+
+import android.app.Application
+
+/**
+ * @author chunping
+ * @time 2023/2/7 11:46 AM
+ * @describe describe
+ */
+class BaseApplication : Application(){
+
+
+    companion object {
+        lateinit var INSTANCE: BaseApplication
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE=this
+    }
+
+}
